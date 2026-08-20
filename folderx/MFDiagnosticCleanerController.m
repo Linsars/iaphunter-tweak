12eb3662c0342f174d5c05d2edf21b1d6edd309d
Linsars <linsars@github.com>
2026-08-20 23:44:41 +0800

    Fix: simplify block structure in MFDiagnosticCleanerController

--- folderx/MFDiagnosticCleanerController.m
+++ folderx/MFDiagnosticCleanerController.m
@@ -55,13 +55,15 @@ static const char *g_diagPaths[] = {
                                                                    message:@"确定要删除所有诊断、分析、崩溃日志和缓存吗？此操作不可撤销。"
                                                             preferredStyle:UIAlertControllerStyleAlert];
     
-    [alert addAction:[UIAlertAction actionWithTitle:@"取消" style:UIAlertActionStyleCancel handler:^(UIAlertAction *action) {
+    UIAlertAction *cancelAction = [UIAlertAction actionWithTitle:@"取消" style:UIAlertActionStyleCancel handler:^(UIAlertAction *action) {
         [self.navigationController popViewControllerAnimated:YES];
-    }]];
+    }];
+    [alert addAction:cancelAction];
     
-    [alert addAction:[UIAlertAction actionWithTitle:@"清理" style:UIAlertActionStyleDestructive handler:^(UIAlertAction *action) {
+    UIAlertAction *cleanupAction = [UIAlertAction actionWithTitle:@"清理" style:UIAlertActionStyleDestructive handler:^(UIAlertAction *action) {
         [self performCleanup];
-    }]];
+    }];
+    [alert addAction:cleanupAction];
     
     [self presentViewController:alert animated:YES completion:nil];
 }
@@ -73,82 +75,92 @@ static const char *g_diagPaths[] = {
     [self presentViewController:loading animated:YES completion:nil];
     
     dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
-        mfLoadLibroot();
-        const char *jbroot = g_libroot_jbrootpath ? g_libroot_jbrootpath() : "";
-        const char *rootfs = g_libroot_rootfspath ? g_libroot_rootfspath() : "";
-        
-        NSMutableArray *targetPaths = [NSMutableArray array];
-        
-        for (int i = 0; g_diagPaths[i]; i++) {
-            NSString *path1 = [NSString stringWithFormat:@"%s%s", jbroot, g_diagPaths[i]];
-            [targetPaths addObject:path1];
-            
-            if (strcmp(jbroot, rootfs) != 0) {
-                NSString *path2 = [NSString stringWithFormat:@"%s%s", rootfs, g_diagPaths[i]];
-                [targetPaths addObject:path2];
-            }
-        }
-        
-        NSSet *uniquePaths = [NSSet setWithArray:targetPaths];
-        
-        NSUInteger deletedCount = 0;
-        NSUInteger failedCount = 0;
-        NSMutableArray *errors = [NSMutableArray array];
-        
-        for (NSString *path in uniquePaths) {
-            NSError *err = nil;
-            BOOL isDir = NO;
-            [[NSFileManager defaultManager] fileExistsAtPath:path isDirectory:&isDir];
-            
-            if (isDir) {
-                NSArray *contents = [[NSFileManager defaultManager] contentsOfDirectoryAtPath:path error:&err];
-                if (contents) {
-                    for (NSString *item in contents) {
-                        NSString *itemPath = [path stringByAppendingPathComponent:item];
-                        NSError *itemErr = nil;
-                        if ([[NSFileManager defaultManager] removeItemAtPath:itemPath error:&itemErr]) {
-                            deletedCount++;
-                        } else {
-                            failedCount++;
-                            [errors addObject:[NSString stringWithFormat:@"%@: %@", itemPath, itemErr.localizedDescription]];
-                        }
-                    }
-                } else if (err) {
-                    failedCount++;
-                    [errors addObject:[NSString stringWithFormat:@"enum %@: %@", path, err.localizedDescription]];
-                }
-            } else {
-                if ([[NSFileManager defaultManager] removeItemAtPath:path error:&err]) {
-                    deletedCount++;
-                } else if (err && err.code != NSFileNoSuchFileError) {
-                    failedCount++;
-                    [errors addObject:[NSString stringWithFormat:@"%@: %@", path, err.localizedDescription]];
-                }
-            }
-        }
-        
-        NSString *msg;
-        if (failedCount == 0) {
-            msg = [NSString stringWithFormat:@"✅ 清理完成，共删除 %lu 项", (unsigned long)deletedCount];
-        } else {
-            msg = [NSString stringWithFormat:@"⚠️ 部分清理失败 (%lu/%lu)，删除了 %lu 项\n错误:\n%@", 
-                   (unsigned long)failedCount, (unsigned long)(deletedCount + failedCount), (unsigned long)deletedCount,
-                   [errors componentsJoinedByString:@"\n"]];
-        }
-        
-        dispatch_async(dispatch_get_main_queue(), ^{
-            [loading dismissViewControllerAnimated:YES completion:^{
-                NSString *resultTitle = (failedCount == 0) ? @"完成" : @"完成 (有失败)";
-                UIAlertController *result = [UIAlertController alertControllerWithTitle:resultTitle
-                                                                                 message:msg
-                                                                          preferredStyle:UIAlertControllerStyleAlert];
-                [result addAction:[UIAlertAction actionWithTitle:@"知道了" style:UIAlertActionStyleDefault handler:^(UIAlertAction *action) {
-                    [self.navigationController popViewControllerAnimated:YES];
-                }]];
-                [self presentViewController:result animated:YES completion:nil];
+        [self performCleanupInBackground:^(NSString *msg, BOOL success) {
+            dispatch_async(dispatch_get_main_queue(), ^{
+                [self showResultAlertWithMessage:msg success:success];
             });
         });
     });
+}
+
+- (void)performCleanupInBackground:(void (^)(NSString *msg, BOOL success))completion {
+    mfLoadLibroot();
+    const char *jbroot = g_libroot_jbrootpath ? g_libroot_jbrootpath() : "";
+    const char *rootfs = g_libroot_rootfspath ? g_libroot_rootfspath() : "";
+    
+    NSMutableArray *targetPaths = [NSMutableArray array];
+    
+    for (int i = 0; g_diagPaths[i]; i++) {
+        NSString *path1 = [NSString stringWithFormat:@"%s%s", jbroot, g_diagPaths[i]];
+        [targetPaths addObject:path1];
+        
+        if (strcmp(jbroot, rootfs) != 0) {
+            NSString *path2 = [NSString stringWithFormat:@"%s%s", rootfs, g_diagPaths[i]];
+            [targetPaths addObject:path2];
+        }
+    }
+    
+    NSSet *uniquePaths = [NSSet setWithArray:targetPaths];
+    
+    NSUInteger deletedCount = 0;
+    NSUInteger failedCount = 0;
+    NSMutableArray *errors = [NSMutableArray array];
+    
+    for (NSString *path in uniquePaths) {
+        NSError *err = nil;
+        BOOL isDir = NO;
+        [[NSFileManager defaultManager] fileExistsAtPath:path isDirectory:&isDir];
+        
+        if (isDir) {
+            NSArray *contents = [[NSFileManager defaultManager] contentsOfDirectoryAtPath:path error:&err];
+            if (contents) {
+                for (NSString *item in contents) {
+                    NSString *itemPath = [path stringByAppendingPathComponent:item];
+                    NSError *itemErr = nil;
+                    if ([[NSFileManager defaultManager] removeItemAtPath:itemPath error:&itemErr]) {
+                        deletedCount++;
+                    } else {
+                        failedCount++;
+                        [errors addObject:[NSString stringWithFormat:@"%@: %@", itemPath, itemErr.localizedDescription]];
+                    }
+                }
+            } else if (err) {
+                failedCount++;
+                [errors addObject:[NSString stringWithFormat:@"enum %@: %@", path, err.localizedDescription]];
+            }
+        } else {
+            if ([[NSFileManager defaultManager] removeItemAtPath:path error:&err]) {
+                deletedCount++;
+            } else if (err && err.code != NSFileNoSuchFileError) {
+                failedCount++;
+                [errors addObject:[NSString stringWithFormat:@"%@: %@", path, err.localizedDescription]];
+            }
+        }
+    }
+    
+    NSString *msg;
+    BOOL success = (failedCount == 0);
+    if (success) {
+        msg = [NSString stringWithFormat:@"✅ 清理完成，共删除 %lu 项", (unsigned long)deletedCount];
+    } else {
+        msg = [NSString stringWithFormat:@"⚠️ 部分清理失败 (%lu/%lu)，删除了 %lu 项\n错误:\n%@", 
+               (unsigned long)failedCount, (unsigned long)(deletedCount + failedCount), (unsigned long)deletedCount,
+               [errors componentsJoinedByString:@"\n"]];
+    }
+    
+    completion(msg, success);
+}
+
+- (void)showResultAlertWithMessage:(NSString *)msg success:(BOOL)success {
+    NSString *resultTitle = success ? @"完成" : @"完成 (有失败)";
+    UIAlertController *result = [UIAlertController alertControllerWithTitle:resultTitle
+                                                                     message:msg
+                                                              preferredStyle:UIAlertControllerStyleAlert];
+    UIAlertAction *okAction = [UIAlertAction actionWithTitle:@"知道了" style:UIAlertActionStyleDefault handler:^(UIAlertAction *action) {
+        [self.navigationController popViewControllerAnimated:YES];
+    }];
+    [result addAction:okAction];
+    [self presentViewController:result animated:YES completion:nil];
 }
 
 @end
